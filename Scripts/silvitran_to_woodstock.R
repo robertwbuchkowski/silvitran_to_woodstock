1d3687255ce077a63db6c23bd9f05ed532815861
Cedric Albert <calbert1@unb.ca>
2022-04-21 15:09:14 -0300

    Just getting things ready here

--- Scripts/silvitran_to_woodstock.R
+++ Scripts/silvitran_to_woodstock.R
@@ -1,2 +1,49 @@
 # This is where the script will be.
 #Hi ROB!
+
+#set R packages
+library(data.table)
+library(spatialEco)
+library(raster)
+library(rasterize)
+library(viridis)
+library(dplyr)
+library(tidyr)
+require(ggplot2)
+library(ggthemes)
+library(rgdal)
+library(sf)#Load shapefiles faster than "rgdal"
+library(sp)
+library(broom)
+library(rgeos)
+library(stars)
+library(nngeo)
+library(EnvStats)
+library(gstat)
+library(paletteer)
+library(ggpubr)
+library(cowplot)
+library(exactextractr)
+
+BFIR_vol00 <- read.csv("BFIR/BFIR_Baseline_volume.csv")
+BFIR_dbh00 <- read.csv("BFIR/BFIR_Baseline_DBH.csv")
+BFIR_hig00 <- read.csv("BFIR/BFIR_Baseline_height.csv")
+
+BFIR_vol45 <- read.csv("BFIR/BFIR_RCP45_volume.csv")
+BFIR_dbh45 <- read.csv("BFIR/BFIR_RCP45_DBH.csv")
+BFIR_hig45 <- read.csv("BFIR/BFIR_RCP45_height.csv")
+
+BFIR_vol85 <- read.csv("BFIR/BFIR_RCP85_volume.csv")
+BFIR_dbh85 <- read.csv("BFIR/BFIR_RCP85_DBH.csv")
+BFIR_hig85 <- read.csv("BFIR/BFIR_RCP85_height.csv")
+
+BFIR_hig00 <- as.data.table(BFIR_hig00)
+
+#summing all columns to get the total stems/ha for each species and age class
+BFIR_hig00[, Tot_stems := Reduce(`+`, .SD), .SDcol = 3:28]
+#cumputing avg weighted hight
+#BFIR_hig00[, avg_hig := (Cls_1*1 + Cls_3*3 + Cls_5*5 + Cls_7*7 + Cls_9*9 + Cls_11*11 + Cls_13*13 +
+#             Cls_15*15 + Cls_17*17 +Cls_19*19 + Cls_21*21 + Cls_23*23 + Cls_25*25 + Cls_27*27 + Cls_29*29 +
+#             Cls_31*31 + Cls_33*33 + Cls_35*35 + Cls_37*37 + Cls_39*39 + Cls_41*41 + Cls_43*43 + 
+#             Cls_45*45 + Cls_47*47 + Cls_49*49 + Cls_gt50*50)/Tot_stems]
+
