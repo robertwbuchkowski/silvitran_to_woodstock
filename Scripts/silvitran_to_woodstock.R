263254f9d9670cf48c1308f928efbc9bec6fc3eb
Cédric Albert <pca6021@umoncton.ca>
2022-04-25 16:04:56 -0300

    Update to integrate height

--- Scripts/silvitran_to_woodstock.R
+++ Scripts/silvitran_to_woodstock.R
@@ -24,6 +24,7 @@ library(paletteer)
 library(ggpubr)
 library(cowplot)
 library(exactextractr)
+library(matrixStats)
 
 BFIR_vol00 <- read.csv("BFIR/BFIR_Baseline_volume.csv")
 BFIR_dbh00 <- read.csv("BFIR/BFIR_Baseline_DBH.csv")
@@ -38,9 +39,42 @@ BFIR_dbh85 <- read.csv("BFIR/BFIR_RCP85_DBH.csv")
 BFIR_hig85 <- read.csv("BFIR/BFIR_RCP85_height.csv")
 
 BFIR_hig00 <- as.data.table(BFIR_hig00)
+BFIR_hig00 <- BFIR_hig00[,-c("Species")]
+
+#number of stem per age class and height class all species combined
+BFIR_hig00 <- aggregate(BFIR_hig00, by=list(Age=BFIR_hig00$Year), FUN=sum)
+BFIR_hig00 <- as.data.table(BFIR_hig00)
+BFIR_hig00 <- BFIR_hig00[,-c("Year")]
+BFIR_hig00 <- BFIR_hig00[,-c("Age")]
+
+#calculate % (in decimals) of the number of trees per height class
+BFIR_hig00$Tot <- rowSums( BFIR_hig00[,1:26] )
+BFIR_hig00 <- as.data.table(BFIR_hig00)
+cols <- c(1:26)
+BFIR_hig00[ , (cols) := lapply(.SD, "/", Tot), .SDcols = cols]
+BFIR_hig00 <- BFIR_hig00[,-c("Tot")]
+
+BFIR_hig00 = t(apply(BFIR_hig00, 1, cumsum))
+
+BFIR_hig00[BFIR_hig00 > 0.899 & BFIR_hig00 < 1]<-1
+
+
+
+
+
+BFIR_hig00 <- as.matrix(BFIR_hig00)
+
+
+few <- apply(BFIR_hig00, 1, function(x) quantile(x[x>=1], probs=.95))
+
+
 
 #summing all columns to get the total stems/ha for each species and age class
-BFIR_hig00[, Tot_stems := Reduce(`+`, .SD), .SDcol = 3:28]
+#BFIR_hig00[, Tot_stems := Reduce(`+`, .SD), .SDcol = 3:28]
+
+
+
+
 #cumputing avg weighted hight
 #BFIR_hig00[, avg_hig := (Cls_1*1 + Cls_3*3 + Cls_5*5 + Cls_7*7 + Cls_9*9 + Cls_11*11 + Cls_13*13 +
 #             Cls_15*15 + Cls_17*17 +Cls_19*19 + Cls_21*21 + Cls_23*23 + Cls_25*25 + Cls_27*27 + Cls_29*29 +
